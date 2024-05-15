b7e71cc118b200a619bb6b338265983338ba45fb
mahmoudamr5896 <mahmoudamr5896@gmail.com>
2024-05-15 21:15:12 +0000

    added project

---
+++ travel_recommendation.js
@@ -0,0 +1,9 @@
+function search() {
+    // Implement your search functionality here
+    alert("Searching...");
+  }
+
+  function reset() {
+    // Implement your reset functionality here
+    document.querySelector('input[type="text"]').value = "";
+  }
